5362f4d050d1ccbf8c22b5aa6c03e87eabc9955b
bperezgo <bsperezgo@gmail.com>
2022-07-03 19:08:43 -0500

    feat(config): pre commit

--- setup.dev.sh
+++ setup.dev.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+
+# Remember change the persmissions of this file
+pre-commit install --hook-type commit-msg
 # TODO: Used to initialize the dev project, and work with the dev config in local
 
 # This command works ok initializing the first topic, over this, will be iterated
-docker exec -it f2fc3c95a6e5 /opt/bitnami/kafka/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --topic events.asset.recorded --partitions 3 --replication-factor 2
+# docker exec -it f2fc3c95a6e5 /opt/bitnami/kafka/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --topic events.asset.recorded --partitions 3 --replication-factor 2
