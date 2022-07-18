97ba617cbbcf24aa721d50dc7efc63719fc1ae48
bperezgo <bsperezgo@gmail.com>
2022-07-17 22:49:46 -0500

    ci: fixing linter setup

---
+++ pipelines/linters/setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "Installing the linters"
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.46.2
--- setup.sh
+++ setup.sh
@@ -3,4 +3,3 @@
 
 echo "Setting up the environment"
 chmod +x ./pipelines/**/*.sh
-go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.46.2
