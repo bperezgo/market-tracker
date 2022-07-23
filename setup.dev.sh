447942f2002d541d4e96abd6297f03d206693234
bperezgo <bsperezgo@gmail.com>
2022-07-23 13:24:58 -0500

    feat: post commit cleanup

--- setup.dev.sh
+++ setup.dev.sh
@@ -5,6 +5,7 @@
 # Remember change the persmissions of this file
 chmod +x ./pipelines/**/*.sh
 pre-commit install
+pre-commit install --hook-type post-commit
 # TODO: Used to initialize the dev project, and work with the dev config in local
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.46.2
 golangci-lint --version
